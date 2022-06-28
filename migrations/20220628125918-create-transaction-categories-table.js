77ca4d304524389780adb1860d547580df30b920
Kimmi Lee <lee.kimmixq@gmail.com>
2022-06-28 21:04:55 +0800

    create migration for transaction_categories table

---
+++ migrations/20220628125918-create-transaction-categories-table.js
@@ -0,0 +1,40 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("transaction_categories", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      category_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: "categories",
+          key: "id",
+        },
+      },
+      txn_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: "transactions",
+          key: "id",
+        },
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable("transaction_categories");
+  },
+};
