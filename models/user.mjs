f2cf12dc099732ee99f377580f08f5932be2a493
Kimmi Lee <lee.kimmixq@gmail.com>
2022-06-28 22:18:07 +0800

    created user model

---
+++ models/user.mjs
@@ -0,0 +1,41 @@
+export default function initUserModel(sequelize, DataTypes) {
+  return sequelize.define(
+    "user",
+    {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      email: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      contact: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      password: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      filename: {
+        type: DataTypes.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+      },
+    },
+    {
+      underscored: true,
+    }
+  );
+}
