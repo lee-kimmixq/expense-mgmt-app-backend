fc1e90fd6b260af5e79bd0f505a74c284106850c
Kimmi Lee <lee.kimmixq@gmail.com>
2022-06-29 22:51:28 +0800

    initiated user controller and added login route

--- routes.mjs
+++ routes.mjs
@@ -2,11 +2,15 @@ import { resolve } from "path";
 import db from "./models/index.mjs";
 
 import initTestController from "./controllers/testController.mjs"; // TEST - TO REMOVE
+import initUserController from "./controllers/users.mjs";
 
 export default function routes(app) {
   // TEST - TO REMOVE
   const TestController = initTestController(db);
   app.get("/test", TestController.test);
+
+  const UserController = initUserController(db);
+  app.post("/login", UserController.login);
 
   // special JS page. Include the webpack index.html file
   app.get("/home", (request, response) => {
