84937e1718ba9b378fd72ae69f918e2ff592e7ad
Kimmi Lee <lee.kimmixq@gmail.com>
2022-07-01 01:05:49 +0800

    removed test controllers and routes

--- routes.mjs
+++ routes.mjs
@@ -2,15 +2,10 @@ import { resolve } from "path";
 import db from "./models/index.mjs";
 import passport from "passport";
 
-import initTestController from "./controllers/testController.mjs"; // TEST - TO REMOVE
 import initUserController from "./controllers/users.mjs";
 import initTransactionController from "./controllers/transactions.mjs";
 
 export default function routes(app) {
-  // TEST - TO REMOVE
-  const TestController = initTestController(db);
-  app.get("/test", TestController.test);
-
   const UserController = initUserController(db);
   app.post("/login", UserController.login);
   app.post("/signup", UserController.signup);
@@ -41,13 +36,6 @@ export default function routes(app) {
     "/transactions/:id",
     passport.authenticate("jwt", { session: false }),
     TransactionController.destroy
-  );
-
-  // test protected route
-  app.get(
-    "/protected",
-    passport.authenticate("jwt", { session: false }),
-    TestController.test
   );
 
   // special JS page. Include the webpack index.html file
