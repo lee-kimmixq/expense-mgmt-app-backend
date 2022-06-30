66eddc1095be226a33b36683c8e8d16e331f1fc1
Kimmi Lee <lee.kimmixq@gmail.com>
2022-07-01 00:59:55 +0800

    created controller function to edit single transaction

--- controllers/transactions.mjs
+++ controllers/transactions.mjs
@@ -53,5 +53,30 @@ export default function initTransactionController(db) {
     }
   };
 
-  return { index, create, show };
+  const update = async (req, res) => {
+    try {
+      const { id: userId } = req.user;
+      const { id: txnId } = req.params;
+      const { amount, txnDate, title, categoryId } = req.body;
+
+      const txn = await db.Transaction.findByPk(txnId);
+      if (!txn) return res.status(400).send("Bad Request");
+      if (userId !== txn.userId) return res.status(403).send("Forbidden"); // return forbidden if transaction doesn't belong to current user
+
+      if (txn.categoryId !== categoryId) await txn.setCategories([categoryId]);
+
+      await txn.update({
+        amount,
+        txnDate,
+        title,
+        updatedAt: new Date(),
+      });
+
+      res.send({ success: true });
+    } catch (err) {
+      res.status(500).send(err);
+    }
+  };
+
+  return { index, create, show, update };
 }
