2213320f4e362781713e10c36b684548e1b2694a
Kimmi Lee <lee.kimmixq@gmail.com>
2022-07-01 00:24:28 +0800

    created controller function to create new transaction

--- controllers/transactions.mjs
+++ controllers/transactions.mjs
@@ -13,5 +13,26 @@ export default function initTransactionController(db) {
     }
   };
 
-  return { index };
+  const create = async (req, res) => {
+    try {
+      const { id } = req.user;
+      const { amount, txnDate, title, categoryId } = req.body;
+
+      const category = await db.Category.findByPk(categoryId);
+      if (!category) throw new Error("Invalid Category");
+
+      const newTxn = await category.createTransaction({
+        userId: id,
+        amount,
+        txnDate,
+        title,
+      });
+
+      res.send({ newTxn });
+    } catch (err) {
+      res.status(500).send(err);
+    }
+  };
+
+  return { index, create };
 }
