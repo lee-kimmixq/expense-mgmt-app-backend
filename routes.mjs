2e7f1041c7d4053f5e52c254389c44381d0e671e
Kimmi Lee <lee.kimmixq@gmail.com>
2022-06-30 14:35:12 +0800

    created test protected route to test auth middleware

--- routes.mjs
+++ routes.mjs
@@ -1,5 +1,6 @@
 import { resolve } from "path";
 import db from "./models/index.mjs";
+import passport from "passport";
 
 import initTestController from "./controllers/testController.mjs"; // TEST - TO REMOVE
 import initUserController from "./controllers/users.mjs";
@@ -11,6 +12,13 @@ export default function routes(app) {
 
   const UserController = initUserController(db);
   app.post("/login", UserController.login);
+
+  // test protected route
+  app.get(
+    "/protected",
+    passport.authenticate("jwt", { session: false }),
+    TestController.test
+  );
 
   // special JS page. Include the webpack index.html file
   app.get("/home", (request, response) => {
