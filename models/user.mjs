c5221447a4b9a3b69828fcdff7df7463b35331b2
Kimmi Lee <lee.kimmixq@gmail.com>
2022-06-30 23:17:14 +0800

    fixed missing username column in user model

--- models/user.mjs
+++ models/user.mjs
@@ -8,6 +8,10 @@ export default function initUserModel(sequelize, DataTypes) {
         primaryKey: true,
         type: DataTypes.INTEGER,
       },
+      username: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
       email: {
         allowNull: false,
         type: DataTypes.STRING,
