804eca11877498bc9a8b55fa8bde241b6c0b95c7
Kimmi Lee <lee.kimmixq@gmail.com>
2022-07-06 01:10:50 +0800

    moved auth.js to utils

--- routes.mjs
+++ routes.mjs
@@ -1,6 +1,6 @@
 import { resolve } from "path";
 import db from "./models/index.mjs";
-import auth from "./middleware/auth.js";
+import auth from "./utils/auth.js";
 
 import initUserController from "./controllers/users.mjs";
 import initTransactionController from "./controllers/transactions.mjs";
@@ -10,14 +10,14 @@ export default function routes(app) {
   app.post("/login", UserController.login);
   app.post("/signup", UserController.signup);
   app.delete("/logout", UserController.logout);
-  app.get("/checkAuth", auth(), UserController.checkAuth);
+  app.get("/checkAuth", auth, UserController.checkAuth);
 
   const TransactionController = initTransactionController(db);
-  app.get("/transactions", auth(), TransactionController.index);
-  app.post("/transactions", auth(), TransactionController.create);
-  app.get("/transactions/:id", auth(), TransactionController.show);
-  app.put("/transactions/:id", auth(), TransactionController.update);
-  app.delete("/transactions/:id", auth(), TransactionController.destroy);
+  app.get("/transactions", auth, TransactionController.index);
+  app.post("/transactions", auth, TransactionController.create);
+  app.get("/transactions/:id", auth, TransactionController.show);
+  app.put("/transactions/:id", auth, TransactionController.update);
+  app.delete("/transactions/:id", auth, TransactionController.destroy);
 
   // special JS page. Include the webpack index.html file
   app.get("/home", (request, response) => {
