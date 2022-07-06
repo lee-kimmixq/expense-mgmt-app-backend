2d12a68deb04e9543fcaafab5401468fd8ecf6e4
Kimmi Lee <lee.kimmixq@gmail.com>
2022-07-07 00:39:30 +0800

    added category router and controller to routes.mjs

--- routes.mjs
+++ routes.mjs
@@ -2,14 +2,18 @@ import db from "./models/index.mjs";
 
 import userRouter from "./routers/userRouter.mjs";
 import transactionRouter from "./routers/transactionRouter.mjs";
+import categoryRouter from "./routers/categoryRouter.mjs";
 
 import initUserController from "./controllers/users.mjs";
 import initTransactionController from "./controllers/transactions.mjs";
+import initCategoryController from "./controllers/categories.mjs";
 
 export default function routes(app) {
   const UserController = initUserController(db);
   const TransactionController = initTransactionController(db);
+  const CategoryController = initCategoryController(db);
 
   app.use("/users", userRouter(UserController));
   app.use("/transactions", transactionRouter(TransactionController));
+  app.use("/categories", categoryRouter(CategoryController));
 }
