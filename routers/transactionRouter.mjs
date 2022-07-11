eea7bae16a9420f74461f01bcebb2e06dd7eea95
Kimmi Lee <lee.kimmixq@gmail.com>
2022-07-12 03:40:51 +0800

    added multerUpload middleware to post txns route

--- routers/transactionRouter.mjs
+++ routers/transactionRouter.mjs
@@ -1,12 +1,13 @@
 import express from "express";
 import auth from "../utils/auth.js";
+import multerUpload from "../utils/s3.js";
 
 const router = express.Router();
 
 export default function transactionRouter(controller) {
   router.get("/reports", auth, controller.report);
   router.get("/", auth, controller.index);
-  router.post("/", auth, controller.create);
+  router.post("/", auth, multerUpload.single("photo"), controller.create);
   router.get("/:id", auth, controller.show);
   router.put("/:id", auth, controller.update);
   router.delete("/:id", auth, controller.destroy);
