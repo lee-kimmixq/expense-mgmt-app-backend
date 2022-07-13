834cc593cc26922e088faa8196de5a1d02a0394d
Kimmi Lee <lee.kimmixq@gmail.com>
2022-07-13 20:06:04 +0800

    added budget router and controller into routes.mjs

--- routes.mjs
+++ routes.mjs
@@ -3,17 +3,21 @@ import db from "./models/index.mjs";
 import userRouter from "./routers/userRouter.mjs";
 import transactionRouter from "./routers/transactionRouter.mjs";
 import categoryRouter from "./routers/categoryRouter.mjs";
+import budgetRouter from "./routers/budgetRouter.mjs";
 
 import initUserController from "./controllers/users.mjs";
 import initTransactionController from "./controllers/transactions.mjs";
 import initCategoryController from "./controllers/categories.mjs";
+import initBudgetController from "./controllers/budgets.mjs";
 
 export default function routes(app) {
   const UserController = initUserController(db);
   const TransactionController = initTransactionController(db);
   const CategoryController = initCategoryController(db);
+  const BudgetController = initBudgetController(db);
 
   app.use("/users", userRouter(UserController));
   app.use("/transactions", transactionRouter(TransactionController));
   app.use("/categories", categoryRouter(CategoryController));
+  app.use("/budgets", budgetRouter(BudgetController));
 }
