f170edd35b25bf6b0bea9d48e2c070bf60bc5004
Kimmi Lee <lee.kimmixq@gmail.com>
2022-07-13 21:53:52 +0800

    added query params and retrieve only latest row for index function

--- controllers/budgets.mjs
+++ controllers/budgets.mjs
@@ -1,11 +1,39 @@
+import sequelize, { Op } from "sequelize";
+
 export default function initBudgetController(db) {
   const index = async (req, res) => {
     try {
       const { id } = req.user;
+      const { showInDashboard } = req.query;
 
-      const budgets = await db.Budget.findAll({
-        where: { userId: id },
-      });
+      const options = {
+        where: {
+          userId: id,
+          id: {
+            [Op.in]: sequelize.literal(
+              `(SELECT MAX(id) FROM budgets GROUP BY category_id)`
+            ),
+          },
+        },
+        attributes: [
+          [
+            sequelize.fn("DISTINCT", sequelize.col("category_id")),
+            "categoryId",
+          ],
+          "id",
+          "amount",
+        ],
+        include: {
+          model: db.Category,
+          attributes: ["name"],
+          where: { isIncome: false },
+        },
+        raw: true,
+      };
+
+      if (showInDashboard) options.where.showInDashboard = true;
+
+      const budgets = await db.Budget.findAll(options);
 
       res.json({ budgets });
     } catch (err) {
