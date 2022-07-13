4a6ff1fd4fc08032067ede24f3cfbf7736d741b5
Kimmi Lee <lee.kimmixq@gmail.com>
2022-07-13 19:53:21 +0800

    created migration for budgets table

---
+++ migrations/20220713114043-create-budgets-table.js
@@ -0,0 +1,48 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("budgets", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      user_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: "users",
+          key: "id",
+        },
+      },
+      category_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: "categories",
+          key: "id",
+        },
+      },
+      amount: {
+        allowNull: false,
+        type: Sequelize.DECIMAL(10, 2),
+      },
+      show_in_dashboard: {
+        allowNull: false,
+        type: Sequelize.BOOLEAN,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable("budgets");
+  },
+};
